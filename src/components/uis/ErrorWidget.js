f1467aeaf27bdf27c6c18e67cd05d388c756b1eb
Praise Afuwape <ayokanmi1275@gmail.com>
2024-01-18 00:01:54 +0100

    updated error widget to reset screen on error

--- src/components/uis/ErrorWidget.js
+++ src/components/uis/ErrorWidget.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function ErrorWidget({ errorText }) {
+function ErrorWidget({ errorText, refresh}) {
     return (
         <div className=' h-screen flex justify-center items-center'>
             <div className='flex flex-col'>
                 <div>Error</div>
                 <div>Failed to {errorText}</div>
+                <button onClick={()=>refresh()}>Go back</button>
             </div>
         </div>
     )
---
+++ src/pages/SignInResTest.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SignInResTest() {
+    return (<></>    );
+}
+
+export default SignInResTest;
